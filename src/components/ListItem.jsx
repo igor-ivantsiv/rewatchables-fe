d83ff716479a47149f0225e25c46e6aacabb0a8d
Igor Ivantsiv <ig.ivantsiv@hotmail.co.uk>
2024-07-08 18:47:59 +0200

    wip

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,40 +1,38 @@
 import { Card, Image, Text, Group, AspectRatio } from "@mantine/core";
-import MoviesDetails from "../pages/MoviesDetails";
-import { useDisclosure } from '@mantine/hooks';
-import { Modal } from '@mantine/core';
-
-const customTitleStyles = {
-  fontSize: '20px',
-  fontWeight: 'bold',
-  color: 'blue',
-};
-
-
+import RewatchableDetails from "./RewatchableDetails";
+import { useDisclosure } from "@mantine/hooks";
+import { Modal } from "@mantine/core";
 
 const ListItem = ({ rewatchable }) => {
   const [opened, { open, close }] = useDisclosure(false);
 
-
   return (
-    <div onClick={open}>
-          <Card shadow="sm" padding="lg" radius="md" withBorder>
-            <Card.Section>
-            <AspectRatio ratio={1080 / 720} maw={366} mx="auto">
-              <Image src={rewatchable.image} height={549} alt="Cover" />
-              </AspectRatio>
-            </Card.Section>
+    <>
+      <Card shadow="sm" padding="lg" radius="md" withBorder onClick={open}>
+        <Card.Section>
+          <AspectRatio ratio={1080 / 720} maw={366} mx="auto">
+            <Image src={rewatchable.image} height={549} alt="Cover" />
+          </AspectRatio>
+        </Card.Section>
 
-            <Group justify="space-between" mt="md" mb="xs">
-              <Text fw={500}>{rewatchable.title}</Text>
-            </Group>
-          </Card>
-          <Modal size ="90%" opened={opened} onClose={close} centered title={rewatchable.title}       classNames={{
-        title: 'customTitle'
-      }}>
-        <MoviesDetails moviesId={rewatchable.id}/>
+        <Group justify="space-between" mt="md" mb="xs">
+          <Text fw={500}>{rewatchable.title}</Text>
+        </Group>
+      </Card>
+    
+      <Modal
+        size="90%"
+        opened={opened}
+        onClose={close}
+        centered
+        title={rewatchable.title}
+        classNames={{
+          title: "customTitle",
+        }}
+      >
+        <RewatchableDetails rewatchableId={rewatchable.id} type={rewatchable.type} />
       </Modal>
-      
-    </div>
+      </>
   );
 };
 
