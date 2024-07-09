f06528bfbb65b649a02443f883c9d95cf44de07d
Igor Ivantsiv <ig.ivantsiv@hotmail.co.uk>
2024-07-09 17:49:45 +0200

    navbar

--- src/AppShellComp.jsx
+++ src/AppShellComp.jsx
@@ -1,40 +1,76 @@
-import { AppShell, Burger } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
-import App from './App';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Header from './components/Header';
+import { ActionIcon, AppShell, Burger } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import App from "./App";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { useState } from "react";
+import {
+  IconLayoutSidebarLeftCollapse,
+  IconLayoutSidebarLeftExpand,
+} from "@tabler/icons-react";
 
 function AppShellComp() {
   const [opened, { toggle }] = useDisclosure();
+
+  const [navbarSmall, setNavbarSmall] = useState(false);
+
+  const navbarWidth = navbarSmall ? 70 : 280;
 
   return (
     <AppShell
       header={{ height: 60 }}
       navbar={{
-        width: 300,
-        breakpoint: 'sm',
+        width: navbarWidth,
+        breakpoint: "sm",
         collapsed: { mobile: !opened },
       }}
       padding="md"
     >
       <AppShell.Header>
-        <Burger
-          opened={opened}
-          onClick={toggle}
-          hiddenFrom="sm"
-          size="sm"
-        />
+        <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
         <Header />
       </AppShell.Header>
 
-      <AppShell.Navbar p="md"><Navbar /></AppShell.Navbar>
+      <AppShell.Navbar p="md">
+        <div className="divNavbar">
+          {navbarSmall ? (
+            <ActionIcon
+              size={32}
+              variant="default"
+              aria-label="ActionIcon with size as a number"
+              radius="xl"
+              onClick={() => setNavbarSmall(false)}
+              className="collapseIcon"
+            >
+              <IconLayoutSidebarLeftExpand />
+            </ActionIcon>
+          ) : (
+            <ActionIcon
+              size={32}
+              variant="default"
+              aria-label="ActionIcon with size as a number"
+              radius="xl"
+              onClick={() => setNavbarSmall(true)}
+              className="collapseIcon"
+            >
+              <IconLayoutSidebarLeftCollapse />
+            </ActionIcon>
+          )}
 
-      <AppShell.Main><App /></AppShell.Main>
+          <Navbar navbarSmall={navbarSmall} />
+        </div>
+      </AppShell.Navbar>
 
-      <AppShell.Footer p="md"><Footer /></AppShell.Footer>
+      <AppShell.Main>
+        <App />
+      </AppShell.Main>
+
+      <AppShell.Footer p="md">
+        <Footer />
+      </AppShell.Footer>
     </AppShell>
   );
 }
 
-export default AppShellComp
+export default AppShellComp;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,56 @@
+import {
+  IconDeviceTv,
+  IconHome,
+  IconMovie,
+  IconUserHeart,
+} from "@tabler/icons-react";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ navbarSmall }) => {
   return (
     <>
       <section className="navbar">
-
-          <div className="menu">
-            <Link className="links" to="/">
-              <p className="menuItem">Home</p>
-            </Link>
-            <Link className="links" to="/movies">
-              <p className="menuItem">Movies</p>
-            </Link>
-            <Link className="links" to="/series">
-              <p className="menuItem">Series</p>
-            </Link>
-            <Link className="links" to="/add">
-              <p className="menuItem">Add Rewatchable</p>
-            </Link>
-            <Link className="links" to="/About">
-              <p className="menuItem">About Us</p>
-            </Link>
-          </div>
+        <div className="menu">
+          {navbarSmall ? (
+            <>
+              <Link className="links" to="/">
+                <p className="navbarP">
+                  <IconHome className="navbarIcon" />
+                </p>
+              </Link>
+              <Link className="links" to="/movies">
+                <p className="navbarP">
+                  <IconMovie className="navbarIcon" />
+                </p>
+              </Link>
+              <Link className="links" to="/series">
+                <p className="navbarP">
+                  <IconDeviceTv className="navbarIcon" />
+                </p>
+              </Link>
+              <Link className="links" to="/About">
+                <p className="navbarP">
+                  <IconUserHeart className="navbarIcon" />
+                </p>
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link className="links" to="/">
+                <p className="menuItem">Home</p>
+              </Link>
+              <Link className="links" to="/movies">
+                <p className="menuItem">Movies</p>
+              </Link>
+              <Link className="links" to="/series">
+                <p className="menuItem">Series</p>
+              </Link>
+              <Link className="links" to="/About">
+                <p className="menuItem">About Us</p>
+              </Link>
+            </>
+          )}
+        </div>
       </section>
     </>
   );
