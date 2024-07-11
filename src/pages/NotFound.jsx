dafaa6c1bc79b19ee976fec2f1e1169e0ff6e9fd
Thomas Kruithof <tkruithof95@gmail.com>
2024-07-11 18:34:31 +0200

    404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,23 @@
+import { AspectRatio, Image } from "@mantine/core";
+import errorImg from "../assets/404.gif"
+
 const NotFound = () => {
   return <>
-  <h1>404</h1>
+  <div className="e404-h-div">
+    <span className="e404-h1">404</span>
+    <span className="e404-h2">Something went wrong...</span>
+  </div>
+  <div className="e404-div">
+  <div className="e404-img-wrap">
+  <AspectRatio ratio={16 /9} maw={600}>
+    <Image
+    src={errorImg}
+    className="e404-gif"
+    alt="404 error"
+    />
+  </AspectRatio>
+  </div>
+  </div>
   </>;
 };
 
