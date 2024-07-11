e40bac9fa1c72209430c6b06a6c6d3c9dd93e609
Thomas Kruithof <tkruithof95@gmail.com>
2024-07-11 19:08:10 +0200

    removed unused files

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
-import AddRewatchable from "./pages/AddRewatchable";
+
 import Suggestions from "./pages/Suggestions";
 import Series from "./pages/Series";
 import Movies from "./pages/Movies";
-import SeriesDetails from "./pages/SeriesDetails";
-import MoviesDetails from "./components/RewatchableDetails";
 
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
-import AddMoviePage from "./pages/AddMoviePage";
-import AddSeriesPage from "./pages/AddSeriesPage";
-import EditPage from "./pages/EditPage";
 import SearchPage from "./pages/SearchPage";
 
 function App() {
@@ -20,19 +15,12 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-
-        <Route path="/add" element={<AddRewatchable />} />
-        <Route path="/add/movie" element={<AddMoviePage />} />
-        <Route path="/add/series" element={<AddSeriesPage />} />
         
         <Route path="/suggestions" element={<Suggestions />} />
         <Route path="/search" element={<SearchPage />} />
 
         <Route path="/series" element={<Series />} />
-        <Route path="/series/:seriesId/edit" element={<EditPage type="series" />} />
-
         <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:moviesId/edit" element={<EditPage type="movies" />} />
 
         <Route path="/about" element={<About />} />
 
